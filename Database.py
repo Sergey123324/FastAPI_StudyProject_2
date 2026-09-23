1219395d44c493edccb204ad16915099d82f997c
Sergey <sergeys1095@gmail.com>
2026-09-23 20:41:29 +0300

    Add migrations

--- Database.py
+++ Database.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 
 DATABASE_URL = "postgresql+psycopg://postgres:fort7859@localhost:5432/fastapi_db"
 engine = create_engine(DATABASE_URL)
-Base.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
